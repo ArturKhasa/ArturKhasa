e213c37a8cad4b3331214f23e25a3ba10880b7ca
Artur <72071780+ArturKhasa@users.noreply.github.com>
2025-03-14 15:12:17 +0300

    Update sshgen.sh

--- sshgen.sh
+++ sshgen.sh
@@ -1,4 +1,10 @@
-ssh-keygen -t rsa -b 4096 -C "arturbk2@yandex.ru"
+#!/bin/bash
+
+# Генерация SSH-ключа
+if [ ! -f ~/.ssh/id_rsa ]; then
+    ssh-keygen -t rsa -b 4096 -C "arturbk2@yandex.ru" -f ~/.ssh/id_rsa -N ""
+fi
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
